e8cb90a0745a31170db11465ed9bb9c774260fae
RJZS <raphael2343@yahoo.co.uk>
2019-02-05 04:34:46 +0000

    Updated utt to noise script for --gen.

--- datasets/out_to_in.py
+++ datasets/out_to_in.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.io import wavfile
+import os
 
 # Script to take the output waveform and turn it into a .npy file with the appropriate form to pass it back in.
 wav_dir = '/home/dawna/tts/rjzs2/noise_results_3t/models-three_tier-three_tier_con.py-expstacked_test_l1-seq_len800-big_fr_sz80-fr_sz20-emb_sz256-skip_connF-dim1024-n_rnn2-rnn_typeGRU-q_levels256-q_typemu-law-bch_sz20-weight_normT-learn_h0T-n_big_rnn2-normed-utt-rmzero-acoustic/samples'
@@ -7,8 +8,9 @@ wav_file_root = 'sample_e15_i100000_t44.18_tr6.4393_v6.5313_best'
 
 out_dir = 'speech/ln_MA_f32_CE_8s_norm_utt'
 
-noise = np.load('{}/speech_test_noise.npy'.format(out_dir))  # Noise to (partially) overwrite.
-for utt in range(5):
+utts = os.listdir('{}'.format(wav_dir))
+noise = np.zeros((len(utts), 128000))
+for utt in range(len(utts)):
     try:
         fs, audio = wavfile.read('{}/{}_{}.wav'.format(wav_dir, wav_file_root, utt))
         audio = audio.astype(np.float32)
@@ -16,9 +18,9 @@ for utt in range(5):
         audio = (audio * 255) / 2
         audio = np.round(audio)
         audio = audio.astype(np.int32)
-        noise[utt, :len(audio)] = audio
+        noise[utt, :] = audio
     except IOError:
         print("Could not find utterance {}. Skipping...".format(utt))
         continue
 
-np.save('{}/speech_test_noise_2.npy'.format(out_dir), noise)
+np.save('{}/generated_noise.npy'.format(out_dir), noise)
