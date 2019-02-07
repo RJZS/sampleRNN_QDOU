a6ec33c706be869cd6e70ef62df177e15ad2521c
Raphael Schmetterling <rjzs2@cam.ac.uk>
2019-02-07 11:34:54 +0000

    Updated out_to_in

--- datasets/out_to_in.py
+++ datasets/out_to_in.py
@@ -3,8 +3,8 @@ from scipy.io import wavfile
 import os
 
 # Script to take the output waveform and turn it into a .npy file with the appropriate form to pass it back in.
-wav_dir = '/home/dawna/tts/rjzs2/noise_results_3t/models-three_tier-three_tier_con.py-expstacked_test_l1-seq_len800-big_fr_sz80-fr_sz20-emb_sz256-skip_connF-dim1024-n_rnn2-rnn_typeGRU-q_levels256-q_typemu-law-bch_sz20-weight_normT-learn_h0T-n_big_rnn2-normed-utt-rmzero-acoustic/samples'
-wav_file_root = 'sample_e15_i100000_t44.18_tr6.4393_v6.5313_best'
+wav_dir = '/home/dawna/tts/rjzs2/noise_results_3t/gen/models-three_tier-three_tier_con.py-expgen_test-seq_len800-big_fr_sz80-fr_sz20-emb_sz256-skip_connF-dim1024-n_rnn2-rnn_typeGRU-q_levels256-q_typemu-law-bch_sz27-weight_normT-learn_h0T-n_big_rnn2-normed-utt-rmzero-acoustic-gen/samples'
+wav_file_root = 'sample_c_noise_random_start.pkl'
 
 out_dir = 'speech/ln_MA_f32_CE_8s_norm_utt'
 
@@ -19,8 +19,9 @@ for utt in range(len(utts)):
         audio = np.round(audio)
         audio = audio.astype(np.int32)
         noise[utt, :] = audio
+        print(utt)
     except IOError:
         print("Could not find utterance {}. Skipping...".format(utt))
         continue
 
-np.save('{}/generated_noise.npy'.format(out_dir), noise)
+np.save('{}/speech_test_noise_1.npy'.format(out_dir), noise)
