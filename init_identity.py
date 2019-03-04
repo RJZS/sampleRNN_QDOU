e5e07ea10264d900d379dff9731235717843f458
RJZS <raphael2343@yahoo.co.uk>
2019-03-04 17:31:11 +0000

    Finished first attempt at manual parameterisation.

--- init_identity.py
+++ init_identity.py
@@ -16,22 +16,35 @@ for key in t.keys():
 
 def top_left_ci(t, key, c, n):
     for i in range(n):
-        t[key][i, i] = c
+        t["{}.W0".format(key)][i, i] = c
+    t["{}.g0".format(key)] = np.linalg.norm(t["{}.W0".format(key)], axis=0)
     return t
 
 
 def concat_top_left_ci(t, key, n, c, start):
     # When multiple weight matrices are concatenated together, and you don't want the top one to be cI.
     for i in range(n):
-        t[key][i, start+i] = c
+        t["{}.W0".format(key)][i, start+i] = c
+    t["{}.g0".format(key)] = np.linalg.norm(t["{}.W0".format(key)], axis=0)
     return t
 
-t = concat_top_left_ci(t, 'BigFrameLevel.GRU1.Step.Input.W0', bfs, c, 2*h)
-t = concat_top_left_ci(t, 'BigFrameLevel.GRU2.Step.Input.W0', bfs, c, 2*h)
 
-t = concat_top_left_ci(t, 'FrameLevel.GRU1.Step.Input.W0', fs, c, 2*h)
-t = concat_top_left_ci(t, 'FrameLevel.GRU2.Step.Input.W0', fs, c, 2*h)
+def split_top_left_ci(t, key, n, c, h, ratio):
+    # For when you expand from higher tier down to a lower one.
+    for r in range(ratio):
+        for i in range(h+r*n, h+(r+1)*n):
+            t["{}.W0".format(key)][i, i] = c
+    t["{}.g0".format(key)] = np.linalg.norm(t["{}.W0".format(key)], axis=0)
+    return t
 
-t = top_left_ci(t, 'BigFrameLevel.Output.W0', c, bfs)
-t = top_left_ci(t, 'FrameLevel.InputExpand.W0', c, fs)
-t = top_left_ci(t, 'FrameLevel.Output.W0', c, fs)
+t = concat_top_left_ci(t, 'BigFrameLevel.GRU1.Step.Input', bfs, c, 2*h)
+t = concat_top_left_ci(t, 'BigFrameLevel.GRU2.Step.Input', bfs, c, 2*h)
+
+t = concat_top_left_ci(t, 'FrameLevel.GRU1.Step.Input', fs, c, 2*h)
+t = concat_top_left_ci(t, 'FrameLevel.GRU2.Step.Input', fs, c, 2*h)
+
+t = top_left_ci(t, 'BigFrameLevel.Output', c, bfs)
+t = top_left_ci(t, 'FrameLevel.Output', c, fs)
+
+t = split_top_left_ci(t, 'BigFrameLevel.Output', bfs, c, h, 4)
+
