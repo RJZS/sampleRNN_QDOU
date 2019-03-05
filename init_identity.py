027a676e4c74a44f0bc4c6cc0c3483e898ac5098
Raphael Schmetterling <rjzs2@cam.ac.uk>
2019-03-05 16:36:13 +0000

    Fixed mistakes and converted to float32.

--- init_identity.py
+++ init_identity.py
@@ -6,9 +6,12 @@ fs = 20  # Frame size
 h = 1024  # Length of hidden state
 c = 0.1
 
-example_pkl_file = 'c_noise_random_start.pkl'  # Use this file to get keys of dictionary and dimensions of each matrix.
+example_pkl_file = 'bkup/c_noise_random_start.pkl'  # Use this file to get keys of dictionary and dimensions of each matrix.
+save_file = 'bkup/identity_transform.pkl'
 with open(example_pkl_file, 'rb') as f:
     t = pickle.load(f)  # Parameters are of type 'float32'.
+
+print("Loaded file!")
 
 for key in t.keys():
     t[key] = np.zeros(t[key].shape)
@@ -17,7 +20,7 @@ for key in t.keys():
 def top_left_ci(t, key, c, n):
     for i in range(n):
         t["{}.W0".format(key)][i, i] = c
-    t["{}.g0".format(key)] = np.linalg.norm(t["{}.W0".format(key)], axis=0)
+    t["{}.g0".format(key)] = np.apply_along_axis(np.linalg.norm,0,t["{}.W0".format(key)])
     return t
 
 
@@ -25,16 +28,18 @@ def concat_top_left_ci(t, key, n, c, start):
     # When multiple weight matrices are concatenated together, and you don't want the top one to be cI.
     for i in range(n):
         t["{}.W0".format(key)][i, start+i] = c
-    t["{}.g0".format(key)] = np.linalg.norm(t["{}.W0".format(key)], axis=0)
+    t["{}.g0".format(key)] = np.apply_along_axis(np.linalg.norm,0,t["{}.W0".format(key)])
     return t
 
 
 def split_top_left_ci(t, key, n, c, h, ratio):
-    # For when you expand from higher tier down to a lower one.
+    # For when you expand from higher tier down to a lower one.i
+    i = 0
     for r in range(ratio):
-        for i in range(h+r*n, h+(r+1)*n):
-            t["{}.W0".format(key)][i, i] = c
-    t["{}.g0".format(key)] = np.linalg.norm(t["{}.W0".format(key)], axis=0)
+        for j in range(h*r, h*r+n):
+            t["{}.W0".format(key)][i, j] = c
+            i += 1
+    t["{}.g0".format(key)] = np.apply_along_axis(np.linalg.norm,0,t["{}.W0".format(key)])
     return t
 
 t = concat_top_left_ci(t, 'BigFrameLevel.GRU1.Step.Input', bfs, c, 2*h)
@@ -43,8 +48,13 @@ t = concat_top_left_ci(t, 'BigFrameLevel.GRU2.Step.Input', bfs, c, 2*h)
 t = concat_top_left_ci(t, 'FrameLevel.GRU1.Step.Input', fs, c, 2*h)
 t = concat_top_left_ci(t, 'FrameLevel.GRU2.Step.Input', fs, c, 2*h)
 
-t = top_left_ci(t, 'BigFrameLevel.Output', c, bfs)
 t = top_left_ci(t, 'FrameLevel.Output', c, fs)
 
-t = split_top_left_ci(t, 'BigFrameLevel.Output', bfs, c, h, 4)
+t = split_top_left_ci(t, 'BigFrameLevel.Output', fs, c, h, int(bfs/fs))
 
+for key in t.keys():
+    t[key] = np.array(t[key]).astype('float32')
+
+print("Saving to file...")
+with open(save_file, 'wb') as handle:
+    pickle.dump(t, handle, protocol=pickle.HIGHEST_PROTOCOL)
